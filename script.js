1b0ead7de3be37746227bec85e8948bab895b1b8
SaiHariVamsi <108571655+SaiHariVamsi@users.noreply.github.com>
2023-11-22 20:45:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 function recordAttendance() {
+    // Get the value entered in the roll number input
     const rollNumber = document.getElementById('rollNumberInput').value;
 
+    // Check if the roll number is not empty
     if (rollNumber.trim() !== '') {
+        // Check if the roll number follows the specified restrictions
         if (isValidRollNumber(rollNumber)) {
-            // Prepare data to send to the server
-            const data = {
-                rollNumber: rollNumber,
-                timestamp: new Date().toISOString(),
-            };
-
-            // Send data to the server using an AJAX request
-            fetch('http://localhost:3000/record-attendance', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            })
-            .then(response => {
-                if (response.ok) {
-                    // Redirect to the success page with a message
-                    window.location.href = `success.html?rollNumber=${rollNumber}`;
-                } else {
-                    throw new Error('Failed to record attendance. Please try again.');
-                }
-            })
-            .catch(error => {
-                alert(error.message);
-            });
+            // Redirect to the success page with a message
+            window.location.href = `success.html?rollNumber=${rollNumber}`;
         } else {
+            // Alert the user if the roll number is invalid
             alert('Invalid roll number. Please follow the specified restrictions.');
         }
     } else {
+        // Alert the user if the roll number is empty
         alert('Please enter your roll number.');
     }
+}
+
+function isValidRollNumber(rollNumber) {
+    // Check if the roll number has exactly 10 characters
+    if (rollNumber.length !== 10) {
+        return false;
+    }
+
+    // Check if the sixth character is an alphabet
+    const sixthCharacter = rollNumber.charAt(5);
+    return /^[a-zA-Z]$/.test(sixthCharacter);
 }
